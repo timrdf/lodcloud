4b91f0dfe10ccb019695d23996f31d81e719eeef
Tim Lebo <timrdf@gmail.com>
2013-04-10 20:17:33 +0000

    During install: added stub directories and readme files.

---
+++ data/source/csv2rdf4lod-source-me-as-lebot.sh
@@ -0,0 +1,12 @@
+#3> <#> a <http://purl.org/twc/vocab/conversion/CSV2RDF4LOD_environment_variables> ;
+#3>     rdfs:seeAlso 
+#3>     <http://purl.org/twc/page/csv2rdf4lod/distributed_env_vars>,
+#3>     <https://github.com/timrdf/csv2rdf4lod-automation/wiki/Script:-source-me.sh> .
+
+export CSV2RDF4LOD_CONVERT_PERSON_URI="http://tw.rpi.edu/instances/TimLebo"
+source /home/lebot/prizms/lodcloud/data/source/csv2rdf4lod-source-me-for-lodcloud.sh
+alias lodcloud='sudo su lodcloud'
+export PATH=$PATH`/home/lebot/opt/prizms/bin/install/paths.sh`
+export CLASSPATH=$CLASSPATH`/home/lebot/opt/prizms/bin/install/classpaths.sh`
+export CSV2RDF4LOD_HOME="/home/lebot/opt/prizms/repos/csv2rdf4lod-automation"
+export JENAROOT=/home/lebot/opt/apache-jena-2.10.0
---
+++ data/source/csv2rdf4lod-source-me-on-lodcloud.sh
@@ -0,0 +1,13 @@
+#3> <#> a <http://purl.org/twc/vocab/conversion/CSV2RDF4LOD_environment_variables> ;
+#3>     rdfs:seeAlso 
+#3>     <http://purl.org/twc/page/csv2rdf4lod/distributed_env_vars>,
+#3>     <https://github.com/timrdf/csv2rdf4lod-automation/wiki/Script:-source-me.sh> .
+# 
+# These shell environment variables apply to a particular machine.
+# By their nature, they are often directory paths for were things are installed.
+
+#export CSV2RDF4LOD_CONVERT_MACHINE_URI="http://tw.rpi.edu/web/inside/machine/aquarius#healthdata"
+#export CSV2RDF4LOD_HOME="/opt/csv2rdf4lod-automation"
+#export CSV2RDF4LOD_CONVERT_DATA_ROOT="/srv/twc-healthdata/data/source"
+#export CSV2RDF4LOD_PUBLISH_VARWWW_ROOT="/var/www"
+#export CSV2RDF4LOD_PUBLISH_LOD_MATERIALIZATION_WWW_ROOT="$CSV2RDF4LOD_PUBLISH_VARWWW_ROOT"
