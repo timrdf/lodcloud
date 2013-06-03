f4dff26aeaf3103e7ce473727f993cae820071ce
Tim Lebo <timrdf@gmail.com>
2013-06-03 15:18:29 +0000

    During Prizms install: added stub directories and readme files.

--- data/source/csv2rdf4lod-source-me-credentials.sh
+++ data/source/csv2rdf4lod-source-me-credentials.sh
@@ -1,1 +1,2 @@
 source /etc/prizms/lodcloud/triple-store/virtuoso/csv2rdf4lod-source-me-for-virtuoso-credentials.sh
+source /etc/prizms/lodcloud/ckan/datahub.io/csv2rdf4lod-source-me-for-ckan-credentials.sh
