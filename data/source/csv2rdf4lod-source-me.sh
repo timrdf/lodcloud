aa8621ecdfc8f72858e5477d035b418dec23ac7e
Tim L <timrdf@gmail.com>
2013-07-07 19:40:29 -0400

    base uri

---
+++ data/source/csv2rdf4lod-source-me.sh
@@ -0,0 +1,3 @@
+#3> <> rdfs:seeAlso <https://github.com/timrdf/csv2rdf4lod-automation/wiki/CSV2RDF4LOD-environment-variables-(considerations-for-a-distributed-workflow)> .
+
+export CSV2RDF4LOD_BASE_URI="http://datafaqstest.tw.rpi.edu"
