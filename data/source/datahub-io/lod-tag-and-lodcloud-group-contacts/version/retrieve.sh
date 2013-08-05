147e3b596156dba06d535d15feb4f6e36bb8e387
Tim L <timrdf@gmail.com>
2013-08-05 12:41:05 -0400

    added temporary publish calls

--- data/source/datahub-io/lod-tag-and-lodcloud-group-contacts/version/retrieve.sh
+++ data/source/datahub-io/lod-tag-and-lodcloud-group-contacts/version/retrieve.sh
@@ -2,3 +2,8 @@
 #
 
 python ../../src/contacts.py ../../src/lod.ttl text/turtle | tee source/lod.ttl
+
+# Short circuit the publishing to a temporary file:
+
+#tar czf contacts.tgz source
+#sudo mv contacts.tgz /var/www/contacts.tgz
