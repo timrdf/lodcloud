7c417042dfba6cef9847c1d2292de96b161cfaee
Tim L <timrdf@gmail.com>
2014-07-15 14:40:15 -0400

    adding TextGetRankedNamedEntities

--- data/source/alchemyapi-com/api-TextGetRankedConcepts/version/2014-Jul-11/retrieve.sh
+++ data/source/alchemyapi-com/api-TextGetRankedConcepts/version/2014-Jul-11/retrieve.sh
@@ -18,7 +18,9 @@ if [[ -n "$X_ALCHEMY_API_Key" ]]; then
    mkdir -p source
    #curl -d '' "$api/calls/text/TextGetRankedConcepts?apikey=$X_ALCHEMY_API_Key&text=The%20City%20by%20the%20Bay%3b%20Fog%20City%3b%20San%20Fran%3b%20Frisco%3b%20The%20City%20that%20Knows%20How%20%3b%20Baghdad%20by%20the%20Bay%20%3b%20The%20Paris%20of%20the%20West&url=http%3A%2F%2Flive.dbpedia.org%2Fpage%2FSan_Francisco&outputMode=rdf" > source/san-francisco-nicknames.rdf
    # ^^ gives &url param \\// does not give &url parameter -- a difference?
-   curl -d '' "$api/calls/text/TextGetRankedConcepts?apikey=$X_ALCHEMY_API_Key&text=The%20City%20by%20the%20Bay%3b%20Fog%20City%3b%20San%20Fran%3b%20Frisco%3b%20The%20City%20that%20Knows%20How%20%3b%20Baghdad%20by%20the%20Bay%20%3b%20The%20Paris%20of%20the%20West&outputMode=rdf" > source/san-francisco-nicknames-nocheating.rdf
+   #curl -d '' "$api/calls/text/TextGetRankedConcepts?apikey=$X_ALCHEMY_API_Key&text=The%20City%20by%20the%20Bay%3b%20Fog%20City%3b%20San%20Fran%3b%20Frisco%3b%20The%20City%20that%20Knows%20How%20%3b%20Baghdad%20by%20the%20Bay%20%3b%20The%20Paris%20of%20the%20West&outputMode=rdf" > source/san-francisco-nicknames-nocheating.rdf
+   api='http://access.alchemyapi.com'
+   curl -d '' "$api/calls/text/TextGetRankedNamedEntities?apikey=$X_ALCHEMY_API_Key&text=The%20City%20by%20the%20Bay%3b%20Fog%20City%3b%20San%20Fran%3b%20Frisco%3b%20The%20City%20that%20Knows%20How%20%3b%20Baghdad%20by%20the%20Bay%20%3b%20The%20Paris%20of%20the%20West&outputMode=rdf" > source/san-francisco-nicknames-entity-extraction.rdf
 else
    echo "ERROR: need to set envvar X_ALCHEMY_API_Key; see http://www.alchemyapi.com/api/calling-the-api/"
 fi
