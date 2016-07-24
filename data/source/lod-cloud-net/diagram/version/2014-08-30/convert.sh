bdd786a4e727507c175225418dfa08b971d6802a
Tim L <timrdf@gmail.com>
2016-07-24 15:35:10 -0400

    convert trigger

---
+++ data/source/lod-cloud-net/diagram/version/2014-08-30/convert.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+#3> <> a conversion:ConversionTrigger; # Could also be conversion:Idempotent;
+#3>    foaf:name    "convert.sh";
+#3>    rdfs:seeAlso <https://github.com/timrdf/csv2rdf4lod-automation/wiki/Triggers#wiki-3-computation-triggers>;
+#3> .
+#
+
+if [[ "$1" == 'clean' ]]; then
+   if [[ "$2" == 'all' ]]; then
+      find -L automatic -name "*colored.svg.ttl" -print0 | \
+         xargs -0 -n 1 -P ${CSV2RDF4LOD_CONCURRENCY:-1} -I converted rm converted
+   else
+      $0 clean 'all'
+   fi
+fi
+
+if [[ -e "$1" ]]; then
+   while [[ $# -gt 0 ]]; do
+      mundane="$1" && shift
+      ttl="automatic/${mundane#source/}.ttl"
+      if [[ ! -e $ttl || $mundane -nt $ttl ]]; then
+         mkdir -p `dirname $ttl`
+         # do some transform here.
+         saxon.sh ../../src/svg.xsl svg ttl $mundane > $ttl
+      fi
+   done
+   exit
+else
+   # https://github.com/timrdf/csv2rdf4lod-automation/wiki/Triggers#parallelize-with-recursive-calls-via-xargs
+   find -L source -name "*colored.svg" -print0 | \
+      xargs -0 -n 1 -P ${CSV2RDF4LOD_CONCURRENCY:-1} -I mundane $0 mundane
+fi
