72e47b52951c84f1b8f5b5ae1de34b1fc6a8fbde
Tim L <timrdf@gmail.com>
2016-07-25 12:11:21 -0400

    new 2014 diagram for positioning and color

--- data/source/us/the-living-lod-cloud/version/convert.sh
+++ data/source/us/the-living-lod-cloud/version/convert.sh
@@ -7,14 +7,17 @@
 #
 
 grddl_local='../../src/svg.xsl'
+grddl_local='../../src/svg.xsl.has-since-been-updated-2016-Jul-24'
 previous_diagram_local='../../src/lod-cloud_colored.svg'
-saxon.sh $grddl_local svg ttl $previous_diagram_local | sed 's/thedatahub.org/datahub.io/g' > automatic/'lod-cloud_colored.svg.ttl'
+previous_diagram_local='../../src/lod-cloud_2014-08-30_colored.svg'
+ttl="`basename $previous_diagram_local`.ttl"
+saxon.sh $grddl_local svg ttl $previous_diagram_local | sed 's/thedatahub.org/datahub.io/g' > automatic/$ttl
 grddl='https://github.com/timrdf/vsr/blob/master/src/xsl/grddl/svg.xsl'
-justify.sh $previous_diagram_local automatic/'lod-cloud_colored.svg.ttl' $grddl
+justify.sh $previous_diagram_local automatic/$ttl $grddl
 
 vsr2grf.sh linksets graffle -w -od automatic source/lodcloud-diagram.rq.rdf
 
-rdf2nt.sh source/'lodcloud-diagram.rq.rdf' automatic/'lod-cloud_colored.svg.ttl' > automatic/'the-living-lod-cloud.nt'
+rdf2nt.sh source/'lodcloud-diagram.rq.rdf' automatic/$ttl > automatic/'the-living-lod-cloud.nt'
 vsr2grf.sh linksets graffle -w automatic/'the-living-lod-cloud.nt'
 #can't b/c two inputs: justify.sh source/'lod-cloud_colored.svg' automatic/'lod-cloud_colored.svg.ttl' $grddl
 
